dd26da1efc21e647c948b8e103832216f751636c
EzeXavier01 <ezexavier103@gmail.com>
2025-02-05 00:18:09 +0100

    Deploy

--- hng1/settings.py
+++ hng1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x9ar#3o1$!2=eaejkr^a73&02g3$uhvpa5g*(g2uo!m-kjqstz'
+SECRET_KEY = os.environ.get("stage1")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost',  ".onrender.com"]
 
 
 # Application definition
